65b5b6e22d0d09bb4dfc754fbc12c2695274f457
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 18:31:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,6 +12,7 @@ class CashRegister
   end
 
   def apply_discount
+    @discount ? nil : return "There is no discount to apply."
     @total *= ((100 - @discount) * 0.01)
     "After the discount, the total comes to $#{@total.to_i}."
   end
