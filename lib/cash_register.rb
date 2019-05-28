ad94cb4f6e056593a5896d1e70d77ea198b3f9f3
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 18:30:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,6 @@ class CashRegister
 
   def apply_discount
     @total *= ((100 - @discount) * 0.01)
-    "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total.to_i}."
   end
 end
