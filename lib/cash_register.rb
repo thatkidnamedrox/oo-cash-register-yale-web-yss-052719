2c3a4e12a93a1842ea9127d9e79eee9aba57d77e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 18:26:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,13 +2,16 @@ class CashRegister
 
   attr_accessor :total, :discount
 
-  def initialize(discount=nil)
+  def initialize(discount=0)
     @total = 0
-    @discount = discount
+    @discount = 0
   end
 
   def add_item(title, price, quantity=1)
     @total += price * quantity
   end
+
+  def apply_discount
+    discount
 
 end
